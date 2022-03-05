ca4192d856c4a0ac78c7e0942becaf20f7300f8c
John Shorthand <ion@Valodea.localdomain>
2022-03-05 19:12:28 +0200

    Adding next_opt

--- Main.cpp
+++ Main.cpp
@@ -3,52 +3,35 @@
 #include "int_types.hpp"
 
 class Option {
-	uint32 opt;
+	uint8 opt[8];
 
 public:
 	uint32 perm_count;
 
 	Option() {
 		// Initilizam cu permutarea 12345678
-		opt = 0b111110101100011010001000u;
+		for(int i = 0;i <= 7;i++)
+			opt[i] = i;
 		perm_count = 0u;
 	}
 
-	void get_array(uint8 *a) {
-		a[0] =  opt        & 0b111u;
-		a[1] = (opt >> 3)  & 0b111u;
-		a[2] = (opt >> 6)  & 0b111u;
-		a[3] = (opt >> 9)  & 0b111u;
-		a[4] = (opt >> 12) & 0b111u;
-		a[5] = (opt >> 15) & 0b111u;
-		a[6] = (opt >> 18) & 0b111u;
-		a[7] = (opt >> 21) & 0b111u;
-	}
-
 	void next_opt() {
-		uint8 a[8];
-		get_array(a);
-		
-		std::next_permutation(a, a + 8);
+		uint8* last = opt + 8;
+		uint8* it = last - 1;
+    	uint8* jt = last;
 
-		opt = a[0] |
-			  a[1] << 3 |
-			  a[2] << 6 |
-			  a[3] << 9 |
-			  a[4] << 12 |
-			  a[5] << 15 |
-			  a[6] << 18 |
-			  a[7] << 21;
-		
+    	while (*it < *--it);
+    	while (*it >= *--jt);
+
+    	std::swap(*it, *jt);
+    	for (++it, --last; it < last; ++it, --last)
+        	std::swap(*it, *last);
 		perm_count++;
 	}
 
 	void print() {
-		uint8 a[8];
-		get_array(a);
-
 		for (int i = 0; i < 8; i++)
-			std::cout << (int)(a[i] + 1);
+			std::cout << (opt[i] + 1);
 		std::cout << '\n';
 	}
 
